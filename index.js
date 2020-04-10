02044b0e1f89063dbd11942e4f5e573059a3a3cc
Jose Pedro Bidegain <josepbidegain@gmail.com>
2020-04-10 18:42:52 -0300

    added services and routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const config = require('./config');
+const videosAPI = require('./routes/videos');
 
 const app = express();
 
+app.use(express.json());
+
+videosAPI(app);
 
 app.use('/hello', (req,res) => {
     res.send('hello');
---
+++ routes/videos.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+
+function videosAPI(app) {
+    const router = express.Router();
+
+    app.use('/api/videos', router);
+    
+    router.get('/', (req, res, next) => {
+        const { tags } = req.query;
+
+        res.status(200).json({
+            data: tags,
+            message: 'Videos list'
+        });
+    });
+
+    router.get('/:videoId', (req, res, next) => {
+        const { videoId } = req.params;
+        res.status(200).json({
+            data: videoId,
+            message: 'Video retrieved'
+        });
+    });
+
+    router.post('/', (req, res, next) => {
+        res.status(201).json({
+            data: {},
+            message: 'Video created'
+        });
+    });
+
+    router.put('/:videoId', (req, res, next) => {
+        const { videoId } = req.params;
+        res.status(200).json({
+            data: videoId, 
+            message: 'Video updated'
+        });
+    });
+
+    router.delete('/:videoId', (req, res, next) => {
+        const { videoId } = req.params;
+        res.status(200).json({
+            data: videoId, 
+            message: 'Video deleted'
+        });
+    });
+}
+
+module.exports = videosAPI;
